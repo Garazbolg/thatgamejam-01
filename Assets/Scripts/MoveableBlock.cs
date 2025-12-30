d5e4c67774cc1a803381e6b02dbebd92dfaa5de5
Garazbolg <garazbolg@gmail.com>
2025-12-30 19:45:51 +0100

    Prevent Moveable block moving into Moveable block

--- Assets/Scripts/MoveableBlock.cs
+++ Assets/Scripts/MoveableBlock.cs
@@ -10,31 +10,12 @@ public class MoveableBlock : MonoBehaviour, IMoveable
         var position = transform.position;
         position.z = 0;
         var hit2D = Physics2D.Raycast(position + direction*0.5f, direction, .5f, collision);
-        
-        if(!hit2D) 
-        {
-            return true;
-        }
-        
-        if(hit2D.collider.TryGetComponent<IMoveable>(out var moveable))
-        {
-            return moveable.CanMove(direction);
-        }
-        
-        return false;
+
+        return !hit2D;
     }
 
     public void Move(Vector3 direction)
     {
-        var position = transform.position;
-        position.z = 0;
-        var hit2D = Physics2D.Raycast(position + direction*0.5f, direction, .5f, collision);
-        
-        if(hit2D && hit2D.collider.TryGetComponent<IMoveable>(out var moveable))
-        {
-            moveable.Move(direction);
-        }
-        
         transform.position += direction;
     }
 }
